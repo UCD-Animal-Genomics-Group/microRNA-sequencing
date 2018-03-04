a63e1eae9b4d748f74935b5b31599c1e74c4c16a
carolcorreia <carolina.correia@ucdconnect.ie>
2018-03-04 18:29:21 +0000

    Iso per canonical field-infected

--- R_scripts/miRNAseq-time-course/isomiRs/03-miRNAseq_isomiRs.R
+++ R_scripts/miRNAseq-time-course/isomiRs/03-miRNAseq_isomiRs.R
@@ -10,7 +10,7 @@
 
 # Authors of current version (2.0.0): Correia, C.N. and Nalpas, N.C.
 # DOI badge of current version:
-# Last updated on 26/02/2018
+# Last updated on 04/03/2018
 
 ############################################
 # 30 Load and/or install required packages #
@@ -45,6 +45,9 @@ load("miRNAseq_isomiRs.RData")
 
 # Set time zone
 Sys.setenv(TZ = "Europe/London")
+
+# Load fonts
+loadfonts()
 
 ####################
 # 32 Fit GLM model #
@@ -336,83 +339,88 @@ dev.off()
 
 # +1 wk
 W1_FDR_05 %>%
-  dplyr::select(-c(starts_with("log"), contains("position"),
-                   starts_with("canon_"),
-                   LR, PValue, FDR)) %>%
-  na.omit() %>%
   dplyr::group_by(canonical_miRNA_ID) %>%
   dplyr::mutate(total_isomirs_per_canonical =
                   length(unique(`IsomiR sequence`))) %>%
-  dplyr::mutate(isomiRs = paste(`IsomiR sequence`, collapse = ",")) %>%
-  dplyr::select(-c(`IsomiR sequence`, mismatch)) %>%
+  dplyr::select(canonical_miRNA_ID, canonical_miRNA_name,
+                total_isomirs_per_canonical) %>%
+  dplyr::arrange(desc(total_isomirs_per_canonical)) %>%
   dplyr::distinct() %>%
-  dplyr::arrange(desc(total_isomirs_per_canonical)) -> iso_W1
+  dplyr::mutate(percent_iso_per_canon =
+                  (total_isomirs_per_canonical /
+                     sum(.$total_isomirs_per_canonical)
+                   * 100)) %>%
+  na.omit() -> iso_W1
 
-View(iso_W1)
+iso_W1
 
 # +2 wk
 W2_FDR_05 %>%
-  dplyr::select(-c(starts_with("log"), contains("position"),
-                   starts_with("canon_"),
-                   LR, PValue, FDR)) %>%
-  na.omit() %>%
   dplyr::group_by(canonical_miRNA_ID) %>%
   dplyr::mutate(total_isomirs_per_canonical =
                   length(unique(`IsomiR sequence`))) %>%
-  dplyr::mutate(isomiRs = paste(`IsomiR sequence`, collapse = ",")) %>%
-  dplyr::select(-c(`IsomiR sequence`, mismatch)) %>%
+  dplyr::select(canonical_miRNA_ID, canonical_miRNA_name,
+                total_isomirs_per_canonical) %>%
+  dplyr::arrange(desc(total_isomirs_per_canonical)) %>%
   dplyr::distinct() %>%
-  dplyr::arrange(desc(total_isomirs_per_canonical)) -> iso_W2
+  dplyr::mutate(percent_iso_per_canon =
+                  (total_isomirs_per_canonical /
+                     sum(.$total_isomirs_per_canonical)
+                   * 100)) %>%
+  na.omit() -> iso_W2
 
-View(iso_W2)
+iso_W2
 
 # +6 wk
 W6_FDR_05 %>%
-  dplyr::select(-c(starts_with("log"), contains("position"),
-                   starts_with("canon_"),
-                   LR, PValue, FDR)) %>%
-  na.omit() %>%
   dplyr::group_by(canonical_miRNA_ID) %>%
   dplyr::mutate(total_isomirs_per_canonical =
                   length(unique(`IsomiR sequence`))) %>%
-  dplyr::mutate(isomiRs = paste(`IsomiR sequence`, collapse = ",")) %>%
-  dplyr::select(-c(`IsomiR sequence`, mismatch)) %>%
+  dplyr::select(canonical_miRNA_ID, canonical_miRNA_name,
+                total_isomirs_per_canonical) %>%
+  dplyr::arrange(desc(total_isomirs_per_canonical)) %>%
   dplyr::distinct() %>%
-  dplyr::arrange(desc(total_isomirs_per_canonical)) -> iso_W6
+  dplyr::mutate(percent_iso_per_canon =
+                  (total_isomirs_per_canonical /
+                     sum(.$total_isomirs_per_canonical)
+                   * 100)) %>%
+  na.omit() -> iso_W6
 
-View(iso_W6)
+iso_W6
 
 # +10 wk
 W10_FDR_05 %>%
-  dplyr::select(-c(starts_with("log"), contains("position"),
-                   starts_with("canon_"),
-                   LR, PValue, FDR)) %>%
-  na.omit() %>%
   dplyr::group_by(canonical_miRNA_ID) %>%
   dplyr::mutate(total_isomirs_per_canonical =
                   length(unique(`IsomiR sequence`))) %>%
-  dplyr::mutate(isomiRs = paste(`IsomiR sequence`, collapse = ",")) %>%
-  dplyr::select(-c(`IsomiR sequence`, mismatch)) %>%
+  dplyr::select(canonical_miRNA_ID, canonical_miRNA_name,
+                total_isomirs_per_canonical) %>%
+  dplyr::arrange(desc(total_isomirs_per_canonical)) %>%
   dplyr::distinct() %>%
-  dplyr::arrange(desc(total_isomirs_per_canonical)) -> iso_W10
+  dplyr::mutate(percent_iso_per_canon =
+                  (total_isomirs_per_canonical /
+                     sum(.$total_isomirs_per_canonical)
+                   * 100)) %>%
+  na.omit() -> iso_W10
 
-View(iso_W10)
+iso_W10
 
 # +12 wk
 W12_FDR_05 %>%
-  dplyr::select(-c(starts_with("log"), contains("position"),
-                   starts_with("canon_"),
-                   LR, PValue, FDR)) %>%
-  na.omit() %>%
   dplyr::group_by(canonical_miRNA_ID) %>%
   dplyr::mutate(total_isomirs_per_canonical =
                   length(unique(`IsomiR sequence`))) %>%
-  dplyr::mutate(isomiRs = paste(`IsomiR sequence`, collapse = ",")) %>%
-  dplyr::select(-c(`IsomiR sequence`, mismatch)) %>%
+  dplyr::select(canonical_miRNA_ID, canonical_miRNA_name,
+                total_isomirs_per_canonical) %>%
+  dplyr::arrange(desc(total_isomirs_per_canonical)) %>%
   dplyr::distinct() %>%
-  dplyr::arrange(desc(total_isomirs_per_canonical)) -> iso_W12
+  dplyr::mutate(percent_iso_per_canon =
+                  (total_isomirs_per_canonical /
+                     sum(.$total_isomirs_per_canonical)
+                   * 100)) %>%
+  na.omit() -> iso_W12
 
-View(iso_W12)
+iso_W12
 
 # Output data
 isoDE <- list(iso_W1, iso_W2, iso_W6, iso_W10, iso_W12)
@@ -425,14 +433,198 @@ pwalk(list(isoDE, isopaths),
       write_csv,
       col_names = TRUE)
 
+###########################################################
+# 39 Plot: number of DE isomiR sequences per canonical ID #
+###########################################################
+
+# +1 wk
+iso_W1 %<>% # Plotting labels
+  unite("label",
+        c(canonical_miRNA_name, canonical_miRNA_ID),
+        sep = " (")
+
+iso_W1$label %<>%
+  str_c(")") %>%
+  factor() %>%
+  fct_inorder()
+
+# Plot
+ggplot(iso_W1) +
+  geom_point(aes(x = total_isomirs_per_canonical,
+                 y = label)) +
+  scale_y_discrete(limits = rev(levels(iso_W1$label))) +
+  theme_bw(base_size = 14, base_family = "Calibri") +
+  ggtitle(paste0("DE isomiRs per canonical miRNA at +1 wk")) +
+  ylab(NULL) +
+  xlab("Total isomiR number") -> iso_W1_plot
+
+iso_W1_plot
+
+
+# +2 wk
+iso_W2 %<>% # Plotting labels
+  unite("label",
+        c(canonical_miRNA_name, canonical_miRNA_ID),
+        sep = " (")
+
+iso_W2$label %<>%
+  str_c(")") %>%
+  factor() %>%
+  fct_inorder()
+
+# Plot
+ggplot(iso_W2) +
+  geom_point(aes(x = total_isomirs_per_canonical,
+                 y = label)) +
+  scale_y_discrete(limits = rev(levels(iso_W2$label))) +
+  theme_bw(base_size = 14, base_family = "Calibri") +
+  ggtitle(paste0("DE isomiRs per canonical miRNA at +2 wk")) +
+  ylab(NULL) +
+  xlab("Total isomiR number") -> iso_W2_plot
+
+iso_W2_plot
+
+
+# +6 wk
+iso_W6 %<>% # Plotting labels
+  unite("label",
+        c(canonical_miRNA_name, canonical_miRNA_ID),
+        sep = " (")
+
+iso_W6$label %<>%
+  str_c(")") %>%
+  factor() %>%
+  fct_inorder()
+
+# Plot
+ggplot(iso_W6) +
+  geom_point(aes(x = total_isomirs_per_canonical,
+                 y = label)) +
+  scale_y_discrete(limits = rev(levels(iso_W6$label))) +
+  theme_bw(base_size = 14, base_family = "Calibri") +
+  ggtitle(paste0("DE isomiRs per canonical miRNA at +6 wk")) +
+  ylab(NULL) +
+  xlab("Total isomiR number") -> iso_W6_plot
+
+iso_W6_plot
+
+
+# +10 wk
+iso_W10 %<>% # Plotting labels
+  unite("label",
+        c(canonical_miRNA_name, canonical_miRNA_ID),
+        sep = " (")
+
+iso_W10$label %<>%
+  str_c(")") %>%
+  factor() %>%
+  fct_inorder()
+
+# Plot
+ggplot(iso_W10) +
+  geom_point(aes(x = total_isomirs_per_canonical,
+                 y = label)) +
+  scale_y_discrete(limits = rev(levels(iso_W10$label))) +
+  theme_bw(base_size = 14, base_family = "Calibri") +
+  ggtitle(paste0("DE isomiRs per canonical miRNA at +10 wk")) +
+  ylab(NULL) +
+  xlab("Total isomiR number") -> iso_W10_plot
+
+iso_W10_plot
+
+
+# +12 wk
+iso_W12 %<>% # Plotting labels
+  unite("label",
+        c(canonical_miRNA_name, canonical_miRNA_ID),
+        sep = " (")
+
+iso_W12$label %<>%
+  str_c(")") %>%
+  factor() %>%
+  fct_inorder()
+
+# Plot
+ggplot(iso_W12) +
+  geom_point(aes(x = total_isomirs_per_canonical,
+                 y = label)) +
+  scale_y_discrete(limits = rev(levels(iso_W12$label))) +
+  theme_bw(base_size = 14, base_family = "Calibri") +
+  ggtitle(paste0("DE isomiRs per canonical miRNA at +12 wk")) +
+  ylab(NULL) +
+  xlab("Total isomiR number") -> iso_W12_plot
+
+iso_W12_plot
+
+
+
+# Export all plots
+ggsave(paste(method, "_DE_iso_canon_plot.pdf", sep = ""),
+       plot      = iso_canon_plot)
+
+Plotfiles <- c(paste0(c("DE-iso_W1", "DE-iso_W2", "DE-iso_W6",
+                       "DE-iso_W10", "DE-iso_W12"),
+                     "_per-canonical.pdf"))
+Plotpaths <- file.path(paste0(imgDir, Plotfiles))
+isoPlots <- list(iso_W1_plot, iso_W2_plot, iso_W6_plot,
+                 iso_W10_plot, iso_W12_plot)
+
+pwalk(list(Plotpaths, isoPlots),
+      ggsave,
+      device    = cairo_pdf,
+      limitsize = FALSE,
+      dpi       = 300,
+      height    = 15,
+      width     = 8,
+      units     = "in")
+
+############################################
+# 40 Plot: Common DE ismoiRs per canonical #
+############################################
+
+iso_W1 %>%
+  dplyr::inner_join(iso_W2,
+                    by = "label",
+                    suffix =c("_W1", "_W2")) %>%
+  dplyr::inner_join(iso_W6,
+                    by = "label") %>%
+  dplyr::inner_join(iso_W10,
+                    by = "label",
+                    suffix =c("_W6", "_W10")) %>%
+  dplyr::inner_join(iso_W12,
+                    by = "label") %>%
+  dplyr::select(-starts_with("percent")) %>%
+  gather(key = "time_point", value = "total_isomirs",
+         starts_with("total_isomirs")) %>%
+  ggplot() +
+  geom_point(aes(x = total_isomirs,
+                 y = label,
+                 colour = time_point),
+             size = 3) +
+  scale_colour_brewer(palette = "BrBG") +
+  scale_y_discrete(limits = rev(levels("label"))) +
+  theme_bw(base_size = 14, base_family = "Calibri") +
+  ggtitle(paste0("Common DE isomiRs per canonical miRNA")) +
+  ylab(NULL) +
+  xlab("Total isomiR number") -> common_iso_plot
+
+ggsave(filename  = paste0(imgDir, "common_iso_plot.pdf"),
+       plot      = common_iso_plot,
+       device    = cairo_pdf,
+       limitsize = FALSE,
+       dpi       = 300,
+       height    = 5,
+       width     = 9,
+       units     = "in")
+
 #######################
-# 39 Save .RData file #
+# 41 Save .RData file #
 #######################
 
 save.image(file = paste0("miRNAseq_", method, ".RData", sep = ""))
 
 ##########################
-# 39 Save R session info #
+# 42 Save R session info #
 ##########################
 
 devtools::session_info()
